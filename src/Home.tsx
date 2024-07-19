030002325a71d4f1138bbd2992b397841a5109e6
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-07-19 14:29:50 +0400

    added README.md

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,14 +6,20 @@ import Image from "./components/Image.tsx";
 export default function Home(): React.JSX.Element {
     return (
         <div className="relative flex h-screen items-center justify-center font-sans">
-            <div className="bg-secondary absolute h-80 w-80 blur-[10rem]" />
+            <div className="absolute h-80 w-80 bg-secondary blur-[10rem]" />
             <div className="z-10">
                 <Image
-                    className="animate-spin-infinite ml-auto mr-auto block h-48 w-48"
+                    className="ml-auto mr-auto block h-48 w-48 animate-spin-infinite"
                     src={`${process.env.PUBLIC_URL}/images/logo.png`}
                     alt="Discontinued Labs Logo"
                 />
                 <h1 className="text-center">React Template</h1>
+                <p className="text-center text-lg">
+                    Created by <a href="https://github.com/discontinuedlabs">Discontinued Labs</a>
+                </p>
+                <p className="text-center text-sm">
+                    <a href="https://github.com/discontinuedlabs/react-template">View on GitHub</a>
+                </p>
             </div>
         </div>
     );
