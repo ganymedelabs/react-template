24c451ef10d4a132001226e0d21588348bcecc57
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-09-19 22:31:02 +0400

    changed to BrowserRouter instead of HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <HashRouter>
+        <Router basename="/react-template">
             <App />
-        </HashRouter>
+        </Router>
     </React.StrictMode>
 );
 
